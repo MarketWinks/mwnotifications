c4355992817126c223e96d08741d117799912dc8
Chocks <chocku.engr@gmail.com>
2020-07-08 14:10:51 +0100

    initial commit

---
+++ src/main/java/com/marketwinks/mwnotifications/MwnotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.marketwinks.mwnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwnotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwnotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketwinks/mwnotifications/MwnotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketwinks.mwnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwnotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
